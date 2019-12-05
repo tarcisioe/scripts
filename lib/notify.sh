1c83043bda349d0e82cda9c1e14982b47d370d64
Tarcísio Eduardo Moreira Crocomo <tcrocomo@esss.co>
2019-12-05 19:11:05 -0300

    Refactor notifications.

---
+++ lib/notify.sh
@@ -0,0 +1,15 @@
+send-notification() {
+    # Send text on the body of a notification.
+    #
+    # Usage: send-notification TEXT [DURATION]
+    #
+    # Positional arguments:
+    #     TEXT       Text to show in the notification.
+    #     DURATION   (Optional) How much time to show the notification for.
+    #                In ms. If omitted, defaults to 1000.
+
+    local text="${1}"
+    local duration="${2:-1000}"
+
+    notify-send -t "${duration}" ' ' "${text}"
+}
