f889b10f1f87631923b2148898d38cdeca6e74f3
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2019-12-05 01:54:14 -0300

    Add lennyface and shrug, refactor clipboard.

---
+++ lib/clipboard.sh
@@ -0,0 +1,13 @@
+set-clipboard() {
+    # Set the X selection and clipboard clipboards to some text.
+    #
+    # Usage: choose FLAG TRUE_VALUE FALSE_VALUE
+    #
+    # Positional arguments:
+    #     TEXT   A text to set the contents of the clipboards.
+
+    local text="${1}"
+
+    printf '%s' "${text}" | xclip
+    printf '%s' "${text}" | xclip -sel c
+}
