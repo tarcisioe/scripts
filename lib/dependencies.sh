917341e495c0848b1f4b7431192a0b3ff2f02a93
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2021-03-15 00:10:24 -0300

    Add check-dependencies.

---
+++ lib/dependencies.sh
@@ -0,0 +1,28 @@
+require path-util
+require ui
+
+
+check-dependencies() {
+    # Check if a list of dependency commands are available.
+    #
+    # Usage: check-dependencies [COMMANDS...]
+    #
+    # Positional arguments:
+    #     COMMANDS   Commands to check for availability.
+
+    local failed=0
+
+    for command in "${@}"
+    do
+        command-available "${command}" || {
+            print-bad "Command ${command} unavailable." \
+                "Please install it to use this script."
+            failed=1
+        }
+    done
+
+    if [[ ${failed} == '1' ]]
+    then
+        die "Some dependencies are not met. Exiting."
+    fi
+}
