e928d796a970c2c63291726f36e1c55dcc1621ab
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2021-06-24 12:03:16 -0300

    Improve and fix nvim wrapper.

--- lib/wrap.sh
+++ lib/wrap.sh
@@ -1,6 +1,25 @@
 require util
 
 
+function get-wrapped-command() {
+    local script_name="$(script-name)"
+
+    local script="$(path-to-script)"
+
+    local possibilities="$(which -a ${script_name})"
+
+    for option in $possibilities
+    do
+        if [[ "$(readlink -f "$option")" != "$script" ]]
+        then
+            echo "$option"
+            break
+        fi
+    done
+
+    return 1
+}
+
 function wrap() {
     # Wrap a command with the same name as the calling script, which comes after
     # it in `which -a`.
@@ -13,20 +32,23 @@ function wrap() {
     #     COMMAND_ARGS   Arguments to be passed to the first command with the
     #                    same name as the calling script that `which` finds.
 
-    local script_name="$(script-name)"
+    local command="$(get-wrapped-command)" || return 1
+    exec "${command}" "$@"
+}
 
-    local script="$(path-to-script)"
 
-    local possibilities="$(which -a ${script_name})"
+function wrap-no-exec() {
+    # Wrap a command with the same name as the calling script, which comes after
+    # it in `which -a`.
+    #
+    # This runs a process (without using exec) on success, or fails with no output.
+    #
+    # Usage: wrap [COMMAND_ARGS...]
+    #
+    # Positional arguments:
+    #     COMMAND_ARGS   Arguments to be passed to the first command with the
+    #                    same name as the calling script that `which` finds.
 
-    for option in $possibilities
-    do
-        if [[ "$(readlink -f "$option")" != "$script" ]]
-        then
-            exec "$option" "$@"
-            break
-        fi
-    done
-
-    return 1
+    local command="$(get-wrapped-command)" || return 1
+    "${command}" "$@"
 }
